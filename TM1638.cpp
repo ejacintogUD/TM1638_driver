7f2721415212f7c40ea7a0161f1fc832a8c019bc
ejacintogUD <159667541+ejacintogUD@users.noreply.github.com>
2024-09-21 20:38:30 -0500

    Update TM1638.cpp

--- TM1638.cpp
+++ TM1638.cpp
@@ -1,74 +1,32 @@
-#include "TM1638.h"
+#ifndef TM1638_H
+#define TM1638_H
 
-TM1638::TM1638(PinName stb, PinName clk, PinName dio) : _stb(stb), _clk(clk), _dio(dio) {
-    _dio.output();
-}
+#include "mbed.h"
+#include <cstdint>
 
-void TM1638::init() {
-    sendCommand(0x8F); // Enciende el display con brillo máximo
-}
+class TM1638 {
+public:
+    TM1638(PinName stb, PinName clk, PinName dio);
+    void init();
+    void setBrightness(uint8_t brightness);
+    void displayDigit(uint8_t position, uint8_t data);
+    void displayLed  (uint8_t led, bool state);
+    void displayNumber(uint32_t number);
+    void clearDisplay();
+    void clearLeds();
+    uint8_t readKeys(); //verificar 
 
-void TM1638::setBrightness(uint8_t brightness) {
-    if (brightness > 7) brightness = 7;
-    sendCommand(0x88 | brightness);
-}
+private:
+    void sendCommand(uint8_t command);
+    void sendData(uint8_t address, uint8_t data);
+    void sendByte(uint8_t data);
 
-void TM1638::displayDigit(uint8_t position, uint8_t data) {
-    sendData(position << 1, data);
-}
+    uint8_t digit[8];
+    const char digitToSegment[10] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 
-void TM1638::displayNumber(uint32_t number) {
-    for (int i = 0; i < 8; i++) {
-        digit[i] = number % 10;
-        displayDigit(i, digitToSegment[digit[7-i]]);
-        number /= 10;
-    }
-}
+    DigitalOut _stb;
+    DigitalOut _clk;
+    DigitalInOut _dio;
+};
 
-void TM1638::clearDisplay() {
-    for (int i = 0; i < 8; i++) {
-        displayDigit(i, 0x00);
-    }
-}
-
-uint8_t TM1638::readKeys() {
-    uint8_t keys = 0;
-    _stb = 0;
-    sendByte(0x42);
-    _dio.input();
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 8; j++) {
-            keys |= (_dio.read() << (i * 8 + j));
-            _clk = 1;
-            wait_us(1);
-            _clk = 0;
-        }
-    }
-    _dio.output();
-    _stb = 1;
-    return keys;
-}
-
-void TM1638::sendCommand(uint8_t command) {
-    _stb = 0;
-    sendByte(command);
-    _stb = 1;
-}
-
-void TM1638::sendData(uint8_t address, uint8_t data) {
-    sendCommand(0x44);
-    _stb = 0;
-    sendByte(0xC0 | address);
-    sendByte(data);
-    _stb = 1;
-}
-
-void TM1638::sendByte(uint8_t data) {
-    for (int i = 0; i < 8; i++) {
-        _dio = (data & 0x01);
-        _clk = 1;
-        wait_us(1);
-        _clk = 0;
-        data >>= 1;
-    }
-}
+#endif
